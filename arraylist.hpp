60d963e719defb2d58de6eee5a1b60e45bb951ce
Matt <mtoal087@gmail.com>
2023-02-28 12:33:13 -0600

    Needs Basic Mutator Operations

---
+++ arraylist.hpp
@@ -0,0 +1,113 @@
+using namespace std;
+
+template <typename T>
+void ArrayList<T>::grow(){
+    T* temp;
+    temp = new T[m_max * 2];
+    for(int i = 0; i < m_size; i++){
+        temp[i] = m_data[i];
+    }
+    m_max = m_max * 2;
+    delete [] m_data;
+    m_data = temp;
+}
+
+template <typename T>
+void ArrayList<T>::shrink(){
+    T* temp;
+    temp = new T[m_max / 2];
+    for(int i = 0; i < m_size; i++){
+        temp[i] = m_data[i];
+    }
+    m_max = m_max / 2;
+    delete [] m_data;
+    m_data = temp;
+}
+
+template <typename T>
+ArrayList<T>::ArrayList(int s, const T& x){
+    while(s > m_max){
+        grow();
+    }
+    m_size = s;
+    T* temp;
+    temp = new T[m_max];
+    for(int i = 0; i < m_size; i++){
+        temp[i] = x;
+    }
+    m_data = temp;
+}
+
+template <typename T>
+ArrayList<T>::~ArrayList(){
+    delete [] m_data;
+    m_data = NULL;
+    m_size = 0;
+    m_max = 0;
+}
+
+template <typename T>
+ArrayList<T>& ArrayList<T>:: operator=(const ArrayList<T>& rhs){
+    delete [] m_data;
+	m_data = new T[rhs.m_max];
+    m_max = rhs.m_max;
+    for(int i = 0; i < rhs.m_size; i++){
+		m_data[i] = rhs.m_data[i];
+	}
+    m_size = rhs.m_size;
+	return *this;
+}
+
+template <typename T>
+ArrayList<T>:: ArrayList(const ArrayList<T>& cpy){
+    m_data = NULL;
+    m_max = m_size = 0;
+    *this = cpy;
+}
+
+template <typename T>
+int ArrayList<T>:: size() const{
+    return m_size;
+}
+
+template <typename T>
+const T& ArrayList<T>::first() const{
+    return m_data[0];
+}
+
+template <typename T>
+T& ArrayList<T>:: operator[](int i){
+    if(i >= 0 && i < m_size)
+        return m_data[i];
+    else{
+        cout << "!Error!" << endl;
+        return m_errobj;
+    }
+}
+
+  // Purpose: return a read-only reference to the element at
+  // the specified position
+  // Parameters: i is 0-based index into the ArrayList
+  // Returns: the element at position i in the ArrayList
+template <typename T>
+const T& ArrayList<T>:: operator[](int i) const{
+    if(i >= 0 && i < m_size)
+        return m_data[i];
+    else{
+        cout << "!Error!" << endl;
+        return m_errobj;
+    }
+}
+
+  // Purpose: finds an element in the ArrayList
+  // Parameters: 'x' is value to be found in the ArrayList
+  // Returns: the position of the first occurrance of 'x' in the list,
+  //          or -1  if 'x' is not found.
+template <typename T>
+int ArrayList<T>::search(const T& x) const{
+    for(int i = 0; i < size; i++){
+        if(m_data[i] == x)
+            return i;
+    }
+    return -1;
+}
